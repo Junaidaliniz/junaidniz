cbb2ad5f9b6244270b7640d7beb13531ae6ca7b7
Junaidaliniz <157386475+Junaidaliniz@users.noreply.github.com>
2026-02-04 16:07:04 +0500

    Update djunaid.js

--- api/djunaid.js
+++ api/djunaid.js
@@ -54,7 +54,7 @@ module.exports = async (req, res) => {
     "X-Requested-With": "XMLHttpRequest",
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "en-US,en;q=0.9",
-    Cookie: "PHPSESSID=rql5lghtfkaeht7l39haolcaoc",
+    Cookie: "PHPSESSID=5jr0ojd29daqe5f7686d3tv8oh",
   };
 
   let url;
