1a75fe9e868a86d866091ef4483b1cd4738950aa
Junaidaliniz <157386475+Junaidaliniz@users.noreply.github.com>
2026-02-26 08:43:43 +0500

    Change BOT_TOKEN to a new value
    Updated the BOT_TOKEN for the application.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from telegram.constants import ParseMode
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-BOT_TOKEN = "8453214909:AAGPy_xyYcGUHcEWnjY0vFmul42gpdJMdM8"
+BOT_TOKEN = "8433897615:AAFGshJ6sD9R_31niIvObq_hExAVXrFmr6A"
 ADMIN_ID = 7011937754
 ADMINS = [7011937754]
 DEFAULT_SMS_API = "https://api-node-js-new-production-b09a.up.railway.app/api?type=sms"
